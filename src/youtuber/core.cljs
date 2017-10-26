b6afa8515ca3a3dd850d9635f57db278c2fc81b0
Conan <aconanlai@gmail.com>
2017-10-25 22:38:34 -0400

    timestamp and form toggle

--- src/youtuber/core.cljs
+++ src/youtuber/core.cljs
@@ -1,14 +1,15 @@
 (ns youtuber.core
-    (:require [reagent.core :as reagent]))
+    (:require [reagent.core :as reagent]
+      [cljs.pprint :refer [cl-format]]))
 
 (enable-console-print!)
-
-(println "This text is printed from src/youtuber/core.cljs. Go ahead and edit it and see reloading in action.")
 
 (def css-transition-group
   (reagent/adapt-react-class js/React.addons.CSSTransitionGroup))
 
-(def app-state (reagent/atom {:time 0 :annotations {2 "hua" 3 "doda" 7 "uhh" 10 "wheeee"}}))
+;; state
+
+(def app-state (reagent/atom {:time 0 :form-open false :annotations {2 "hua" 3 "doda" 7 "uhh" 10 "wheeee"}}))
 
 (defn within-range
   [middle key v]
@@ -17,21 +18,45 @@
   
 (def shown-annotations (reagent.ratom/reaction (reverse (filter (partial within-range (@app-state :time)) (@app-state :annotations)))))
 
+;; funcs
+
+(defn convert-timestamp
+  [seconds]
+  (str (int (/ seconds 60)) ":" (cl-format nil "~2,'0d" (mod seconds 60))))
+
+(defn toggle-form
+  []
+  (swap! app-state update-in [:form-open] #(not %)))
+
+;; components
+
 (defn annotation-panel [[time comment]]
   [:div.annotation {:key time}
-   [:span.time time ":"] 
+   [:span.time (convert-timestamp time) ":"] 
    [:span.comment comment]])
-  
+
+(defn add-button []
+  (if (not (@app-state :form-open))
+    [:button {:on-click toggle-form} "Add Annotation"]))
+
+(defn add-form []
+  (if (@app-state :form-open)
+    [:span "hello"]))
+
 (defn app []
   [:div.wrapper
    [:h1.title (@app-state :time)]
    [:div#video]
+   [add-form]
+   [add-button]
    [:div.annotations-container
     [css-transition-group {:transition-name "annotation"}
      (map annotation-panel @shown-annotations)]]])
 
 (reagent/render-component [app]
                       (. js/document (getElementById "app")))
+
+;; youtube component and app timer
 
 (def youtube
   (let [Player (.-Player js/YT)]
